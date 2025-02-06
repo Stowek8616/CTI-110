73e0f2e7a84e9a6818154c7fe692d6bac9544a23
Stowek8616 <143423184+Stowek8616@users.noreply.github.com>
2025-02-06 14:18:14 -0500

    Add files via upload

---
+++ P1LAB1_StoweKyle.py
@@ -0,0 +1,10 @@
+# Kyle Stowe
+# 2-6-2025
+# P1LAB1
+# This program will get your name and age and welcome you to the course
+
+fname = input("Enter your first name: ")
+
+lname = input("Enter your last name: ")
+
+print("Hello, "+str(fname)+" "+str(lname)+"! Welcome to CTI-110")
