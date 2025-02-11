95834f15dadadda9da7e0ca4038d6db732e1b9a2
Stowek8616 <143423184+Stowek8616@users.noreply.github.com>
2025-02-11 13:55:01 -0500

    Update P1HW1_StoweKyle.py

--- P1HW1_StoweKyle.py
+++ P1HW1_StoweKyle.py
@@ -1,1 +1,25 @@
+# Kyle
+# 2/11/2025
+# P1HW1
+
+print("\n------Calculating Exponents------")
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print("\n")
+
+final = float(base**exponent)
+print(base, "raised to the power of", exponent, "is", final)
+
+
+print("\n\n------Addition and Subtraction------")
+start = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
+print("\n")
+
+result = int(start + add - subtract)
+print(start, "+", add, "-", subtract, "is equal to", result)
+
+print("\n")
+input("Press any key to continue")
 
