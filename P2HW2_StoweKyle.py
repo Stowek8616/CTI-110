cd72d0a030ba66988b62ebb57b84c558e792fcf8
Stowek8616 <143423184+Stowek8616@users.noreply.github.com>
2025-02-27 14:26:40 -0500

    Add files via upload

---
+++ P2HW2_StoweKyle.py
@@ -0,0 +1,12 @@
+# Kyle Stowe
+# 2/27/2025
+# P2HW2
+# asks for grades for each module and stores the grades with info such as average grade and sum of grades
+
+module1 = float(input("Enter grade for Module 1:")
+module2 = float(input("Enter grade for Module 2:")
+module3 = float(input("Enter grade for Module 3:")
+module4 = float(input("Enter grade for Module 4:")
+module5 = float(input("Enter grade for Module 5:")
+module6 = float(input("Enter grade for Module 6:")
+
