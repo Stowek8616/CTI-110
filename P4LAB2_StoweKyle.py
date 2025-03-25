6cfc9a0d9aace96997ff8a24415b4573dbdd13ca
Stowek8616 <143423184+Stowek8616@users.noreply.github.com>
2025-03-25 14:16:20 -0400

    Update P4LAB2_StoweKyle.py

--- P4LAB2_StoweKyle.py
+++ P4LAB2_StoweKyle.py
@@ -1,6 +1,10 @@
 keepGoing = "yes"
 while keepGoing.lower() =="yes":
-    num = int(input("Enter an integer: "))
+    num = int(input("Enter an integer 12 or less: "))
+    while num < 0 or num > 12:
+        print("Invalid Entry!")
+        print("Input must be a positive number that is 12 or less")
+        num = int(input("Enter an integer 12 or less: "))
     print()
     if(num >= 0):
         for i in range(1,12+1):
